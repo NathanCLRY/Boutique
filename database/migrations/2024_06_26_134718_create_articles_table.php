7c5b70733c6ff2fada9292ae8d1bf4e3199f2ada
NathanCLRY <nathan.charlery@gmail.com>
2024-06-26 10:08:41 -0400

    Create models and migrations

---
+++ database/migrations/2024_06_26_134718_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class)->constrained()->onDelete('cascade');
+            $table->string('name', 60);
+            $table->string('brand', 60);
+            $table->float('price');
+            $table->string('image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
